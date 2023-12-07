5279a76e2ec2d1cf4833bdadd05788646d188bbb
stef663k <stef663k@elev.techcollege.dk>
2023-12-07 14:55:23 +0100

    first commit

---
+++ bullet.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+# var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _process(delta):
+	# Add the gravity.
+	# if not is_on_floor():
+	# 	velocity.y += gravity * delta
+	velocity = Vector2.from_angle(rotation)*SPEED# * SPEED * delta
+	print(velocity)
+	
+	move_and_slide()
---
+++ test.gd
@@ -0,0 +1,20 @@
+extends Node2D
+var bullet = preload("res://bullet.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_shooting(pos, angle):
+	var b = bullet.instantiate()
+	b.global_position = pos
+	b.rotation = angle - PI / 2
+	add_child(b)
+	
+	
